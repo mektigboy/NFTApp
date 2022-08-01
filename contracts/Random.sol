f29d1ae2c44adc5c668c8ca8f06a646d7435d532
mektigboy <mektigboy@protonmail.com>
2022-08-01 13:27:50 -0500

    Update contracts/Random.sol

--- contracts/Random.sol
+++ contracts/Random.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.15;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
 /// @title Random IPFS NFT
 /// @author mektigboy
@@ -9,6 +11,36 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 /// @dev This contract utilizes Chainlink VRF v2 for randomness.
 /// URI points to IPFS.
 /// Imports contracts from OpenZeppelin.
-contract Random is ERC721URIStorage {
-    constructor() ERC721("Random IPFS NFT", "RIN") {}
+contract Random is ERC721URIStorage, VRFConsumerBaseV2 {
+    VRFCoordinatorV2Interface immutable i_vrfCoordinator;
+    bytes32 immutable i_gasLane;
+    uint64 immutable i_subscriptionId;
+    uint32 immutable i_callbackGasLimit;
+    uint16 constant REQUEST_CONFIRMATIONS = 3;
+    uint32 constant NUMBER_WORDS = 3;
+
+    constructor(
+        address vrfCoordinatorV2,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) ERC721("Random IPFS NFT", "RIN") VRFConsumerBaseV2(vrfCoordinatorV2) {
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
+    }
+
+    // Mint a random object:
+
+    // 1. Get random number.
+    function requestObject() public returns (uint256 requestId) {
+        requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUMBER_WORDS
+        );
+    }
 }
